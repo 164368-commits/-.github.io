51b73e32ae5846091cc018c906977432309f577f
Rp1331 <164368@wsk.ac.th>
2026-07-27 20:58:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
 const music = document.getElementById("music");
 
 const images = [
-
 "751035036_1719591268958655_8370233642973154637_n.jpg",
-
 "752550920_1018062320995628_3834456038027307601_n.jpg",
-
 "754015096_2926203654416033_1176873942568398260_n.jpg",
-
 "753105080_1500194058456130_231933915178450339_n.jpg",
-
 "751134574_1477406654429882_498575148599167075_n.jpg"
-
 ];
-
 
 let index = 0;
 
 
-// ปุ่มเริ่มเซอร์ไพรส์
 document.getElementById("startBtn").onclick = function(){
 
     let count = 3;
 
     let countdown = document.getElementById("countdown");
 
+    // ซ่อนปุ่ม
     this.style.display = "none";
 
+    // แสดงเลขแรก
     countdown.innerHTML = count;
 
 
-
-    let timer = setInterval(()=>{
+    let timer = setInterval(function(){
 
 
         count--;
@@ -39,49 +32,45 @@ document.getElementById("startBtn").onclick = function(){
         countdown.innerHTML = count;
 
 
-
         if(count === 0){
-
 
             clearInterval(timer);
 
 
-            // ซ่อนหน้ากดปุ่ม
-            document.getElementById("startScreen").style.display="none";
+            // รอแป๊บก่อนเปิด
+            setTimeout(()=>{
 
 
-            // แสดงการ์ด
-            document.getElementById("content").style.display="block";
+                document.getElementById("startScreen").style.display="none";
+
+                document.getElementById("content").style.display="block";
 
 
-            // เล่นเพลง
-            music.play();
-
-
-
-            // เปลี่ยนรูปทุก 5 วิ
-            setInterval(()=>{
-
-
-                index++;
-
-
-                if(index >= images.length){
-
-                    index = 0;
-
-                }
-
-
-                document.getElementById("slide").src = images[index];
-
-
-            },5000);
+                // เล่นเพลง
+                music.play();
 
 
 
-            // สร้างหัวใจ
-            setInterval(createHeart,250);
+                // เปลี่ยนรูป
+                setInterval(()=>{
+
+                    index++;
+
+                    if(index >= images.length){
+                        index = 0;
+                    }
+
+                    document.getElementById("slide").src = images[index];
+
+                },5000);
+
+
+
+                // หัวใจลอย
+                setInterval(createHeart,250);
+
+
+            },500);
 
 
         }
@@ -89,43 +78,31 @@ document.getElementById("startBtn").onclick = function(){
 
     },1000);
 
-
 };
 
 
 
-// สร้างหัวใจลอย
+// หัวใจลอย
 function createHeart(){
-
 
     const heart = document.createElement("div");
 
-
     heart.className="float";
-
 
     heart.innerHTML="💖";
 
-
     heart.style.left=Math.random()*100+"vw";
-
 
     heart.style.fontSize=(20+Math.random()*40)+"px";
 
-
     heart.style.animationDuration=(4+Math.random()*5)+"s";
-
 
 
     document.body.appendChild(heart);
 
 
-
     setTimeout(()=>{
-
         heart.remove();
-
     },9000);
-
 
 }
